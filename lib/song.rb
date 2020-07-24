650b9002feae5c31396d87e2009f2e345d7f4376
Dalia Sawaya <dalia.sawaya@flatironschool.com>
2020-07-24 20:40:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-        genre_count[genre] ||= 1 
+        genre_count[genre] ||= 0 
         genre_count[genre] += 1
     end
     genre_count
